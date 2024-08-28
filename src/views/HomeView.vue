113cd85b534a9ddc736965595e99e5ef48687e79
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-08-28 00:12:21 -0300

    edit-home-view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,33 @@
 <script setup lang="ts">
+import HeaderComponent from '@/components/HeaderComponent.vue'
+import MentorFormComponent from '@/components/MentorFormComponent.vue'
+import MentorsTableComponent from '@/components/MentorsTableComponent.vue'
+
+import { ref } from 'vue'
+
+const search = ref<string>('')
 </script>
 
 <template>
+  <HeaderComponent />
+
   <main>
+    <v-container>
+      <v-row class="d-flex align-center justify-space-evenly">
+        <v-col cols="12" sm="6" md="8" lg="9" class="mx-0">
+          <v-text-field
+            v-model="search"
+            label="Pesquisar"
+            prepend-inner-icon="mdi-magnify"
+            variant="solo"
+            flat
+            hide-details
+            single-line
+          ></v-text-field>
+        </v-col>
+        <MentorFormComponent />
+        <MentorsTableComponent />
+      </v-row>
+    </v-container>
   </main>
 </template>
